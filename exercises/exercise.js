fa5c015ae0fa5641c0255f09f9e427e32630c0c0
MarcelloDeidda <marcello35@hotmail.it>
2023-06-20 13:12:58 +0100

    New exercise handler

--- exercises/exercise.js
+++ exercises/exercise.js
@@ -1,27 +1,60 @@
 const prompt = require("prompt");
+const Note = require("../utils/notes/note");
+const IntervalCalculator = require("../utils/intervals/interval-calculator");
+const Interval = require("../utils/intervals/interval");
 
-const Exercise = class {
-    constructor({ question, answers }) {
-        this.question = question;
-        this.answers = answers;
-    }
-
-    async execute() {
+const ExerciseHandler = class {
+    static async execute({ question, answers }) {
         while (true) {
-            console.log(this.question);
+            console.log(question);
             prompt.start();
             const { answer } = await prompt.get(["answer"]);
 
-            if (this.answers.includes(answer)) {
+            if (answers.includes(answer)) {
                 console.log("Correct!");
                 break;
             } else {
                 console.log("Wrong!");
-                console.log(`The correct answer is: ${this.answers}`);
+                console.log(`The correct answer is: ${answers}`);
                 break;
             }
         }
     }
+
+    static removeAccidentals(notes) {
+        const newNotes = notes.map(note => {
+            return new Note(`${note.name}${note.octave}`)
+        })
+
+        return newNotes;
+    }
+
+    static tamperChord(notes) {
+        let newNotes = notes.slice();
+        let newNoteIndex = Math.floor(Math.random() * notes.length);
+        let asc = Math.floor(Math.random() * 2) === 1 ? true : false;
+        
+        let newNote = IntervalCalculator.calculateNoteFromInterval(newNotes[newNoteIndex], new Interval("1 augmented"), asc);
+        newNotes[newNoteIndex] = newNote;
+        return newNotes;
+    }
+
+    static tamperChordNoDouble(notes) {
+        let newNotes = notes.slice();
+        
+        let asc = Math.floor(Math.random() * 2) === 1 ? true : false;
+        let newNoteIndex = Math.floor(Math.random() * notes.length);
+        let newNote = IntervalCalculator.calculateNoteFromInterval(newNotes[newNoteIndex], new Interval("1 augmented"), asc);
+        
+        while (newNote.alterationInSemitones < -1 || newNote.alterationInSemitones > 1) {
+            asc = Math.floor(Math.random() * 2) === 1 ? true : false;
+            newNoteIndex = Math.floor(Math.random() * notes.length);
+            newNote = IntervalCalculator.calculateNoteFromInterval(newNotes[newNoteIndex], new Interval("1 augmented"), asc);
+        }
+        
+        newNotes[newNoteIndex] = newNote;
+        return newNotes;
+    }
 }
 
-module.exports = Exercise;
+module.exports = ExerciseHandler;
