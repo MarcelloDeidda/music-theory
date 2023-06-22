958c24d9f3073cdae569a0f13e0cfb329bff023a
MarcelloDeidda <marcello35@hotmail.it>
2023-06-22 22:34:06 +0100

    Fixed chords-functions refactor

---
+++ utils/chords/chords-functions.js
@@ -0,0 +1,74 @@
+const IntervalCalculator = require("../intervals/interval-calculator");
+const Interval = require("../intervals/interval");
+const Note = require("../notes/note");
+const { printKeyboard } = require("../notes/notes-utils");
+
+module.exports.majorChord = root => {
+    let third = IntervalCalculator.calculateNoteFromInterval(root, new Interval("3 major"));
+    let fifth = IntervalCalculator.calculateNoteFromInterval(root, new Interval("5 perfect"));
+
+    return [root, third, fifth];
+}
+
+module.exports.minorChord = root => {
+    // The parameter "root" MUST be a Note object
+    let third = IntervalCalculator.calculateNoteFromInterval(root, new Interval("3 minor"));
+    let fifth = IntervalCalculator.calculateNoteFromInterval(root, new Interval("5 perfect"));
+
+    return [root, third, fifth];
+}
+
+module.exports.diminishedChord = root => {
+    // The parameter "root" MUST be a Note object
+    let third = IntervalCalculator.calculateNoteFromInterval(root, new Interval("3 minor"));
+    let fifth = IntervalCalculator.calculateNoteFromInterval(root, new Interval("5 diminished"));
+
+    return [root, third, fifth];
+}
+
+module.exports.augmentedChord = root => {
+    // The parameter "root" MUST be a Note object
+    let third = IntervalCalculator.calculateNoteFromInterval(root, new Interval("3 major"));
+    let fifth = IntervalCalculator.calculateNoteFromInterval(root, new Interval("5 augmented"));
+
+    return [root, third, fifth];
+}
+
+module.exports.dominantSeventh = root => {
+    // The parameter "root" MUST be a Note object
+    let third = IntervalCalculator.calculateNoteFromInterval(root, new Interval("3 major"));
+    let fifth = IntervalCalculator.calculateNoteFromInterval(root, new Interval("5 perfect"));
+    let seventh = IntervalCalculator.calculateNoteFromInterval(root, new Interval("7 minor"));
+
+    return [root, third, fifth, seventh];
+}
+
+module.exports.triadFromKey = (degree, tonic, keySignature) => {
+    const keyboard = printKeyboard();
+
+    let tonicIndex = keyboard.indexOf(`${tonic.name}${tonic.octave}`);
+
+    let root = keyboard[tonicIndex + degree - 1];
+    let third = keyboard[tonicIndex + degree + 2 - 1];
+    let fifth = keyboard[tonicIndex + degree + 4 - 1];
+
+    for (let acc of keySignature) {
+        if (root.slice(0, root.length - 1) === acc[0]) {
+            root = acc + root.slice(root.length - 1);
+        }
+        
+        if (third.slice(0, third.length - 1) === acc[0]) {
+            third = acc + third.slice(third.length - 1);
+        }
+        
+        if (fifth.slice(0, fifth.length - 1) === acc[0]) {
+            fifth = acc + fifth.slice(fifth.length - 1);
+        }
+    }
+
+    return [
+        new Note(root),
+        new Note(third),
+        new Note(fifth)
+    ];
+}
