eed7216abfc282eba0b3d7b7241b688976c37b44
MarcelloDeidda <marcello35@hotmail.it>
2023-07-01 13:44:02 +0100

    Fixed chromatic scale function

--- utils/keys/scales-functions.js
+++ utils/keys/scales-functions.js
@@ -1,5 +1,6 @@
 const Interval = require("../intervals/interval");
 const { calculateNoteFromInterval } = require("../intervals/intervals-functions");
+const { printKeyboard} = require("../../utils/notes/notes-functions")
 
 const Note = require("../notes/note");
 
@@ -26,30 +27,53 @@ module.exports.scaleFromKey = (tonic, keySignature) => {
 
 module.exports.createChromaticScale = (startingNote, asc = true) => {
     const intervals = ["augmented 1", "minor 2"];
-    const chromaticScale = [];
+    let chromaticScale = [];
 
     chromaticScale.push(startingNote);
 
-    for (let i = 0; i < 11; i++) {
+    while (chromaticScale.length < 12) {
         let random = Math.floor(Math.random() * 2);
         let nextNote = calculateNoteFromInterval(chromaticScale[chromaticScale.length - 1], new Interval(intervals[random]), asc);
 
         while (
             nextNote.getNoteWithoutOctave === "undefined" ||
-            (
-                chromaticScale.length > 2 &&
-                chromaticScale[chromaticScale.length - 1].getLetterName() === nextNote.getLetterName() &&
-                chromaticScale[chromaticScale.length - 2].getLetterName() === nextNote.getLetterName()
-            ) ||
             (nextNote.getAccidentalInSemitones() < -1 || nextNote.getAccidentalInSemitones() > 1)
         ) {
             random = random === 0 ? 1 : 0;
             nextNote = calculateNoteFromInterval(chromaticScale[chromaticScale.length - 1], new Interval(intervals[random]), asc);
         }
+        
+        if (
+            (
+                chromaticScale.length > 1 &&
+                chromaticScale[chromaticScale.length - 1].getLetterName() === nextNote.getLetterName() &&
+                chromaticScale[chromaticScale.length - 2].getLetterName() === nextNote.getLetterName()
+            ) ||
+            (
+                chromaticScale.length === 11 &&
+                chromaticScale[chromaticScale.length - 1].getLetterName() === nextNote.getLetterName() &&
+                chromaticScale[0].getLetterName() === nextNote.getLetterName()
+            ) ||
+            (
+                chromaticScale.length === 11 &&
+                (
+                    (asc && ![-1, 0, 6].includes(nextNote.getLetterName().charCodeAt() - startingNote.getLetterName().charCodeAt())) ||
+                    (!asc && ![1, 0, -6].includes(nextNote.getLetterName().charCodeAt() - startingNote.getLetterName().charCodeAt())))
+            ) 
+        ) {
+            chromaticScale.pop();
+
+            if (chromaticScale.length > 2) {
+                chromaticScale.pop();
+            }
+
+            continue;
+        }
 
         chromaticScale.push(nextNote);
     }
-    chromaticScale.push(calculateNoteFromInterval(startingNote, new Interval("perfect 8"), asc));
     
+    chromaticScale.push(calculateNoteFromInterval(startingNote, new Interval("perfect 8"), asc));
+
     return chromaticScale;
 }
