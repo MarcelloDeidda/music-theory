261e90808546f7dd50b0ee6b9dcf121c6d110d8a
MarcelloDeidda <marcello35@hotmail.it>
2023-07-04 22:16:19 +0100

    New class RhythmicGroup and function subdivideNote

--- music-theory.js
+++ music-theory.js
@@ -2,8 +2,10 @@
 
 const KeyExercises = require("./exercises/scales/key-exercises");
 const Note = require("./utils/notes/note");
-const { calculateIimeSignatureValue, checkBarBeats, getRandomTimeSignature, createBar } = require("./utils/rhythm/rhythm-utils");
+const { calculateIimeSignatureValue, checkBarBeats, getRandomTimeSignature, createBar, subdivideNote } = require("./utils/rhythm/rhythm-utils");
 
 const notes = [new Note("C4", "minim", true), new Note("C4", "crotchet")/*, new Note("C4"), new Note("C4"), new Note("C4")*/];
 
-console.log(createBar([7, 4]));
+const group = subdivideNote("semibreve", "semiquaver", 4);
+
+group.print()
--- utils/rhythm/rhythm-utils.js
+++ utils/rhythm/rhythm-utils.js
@@ -86,3 +86,100 @@ module.exports.createBar = (timeSignature = [4, 4], numOfBars = 1) => {
 
     return;
 }
+
+module.exports.RhythmicGroup = class {
+    #notes;
+    #groupValue;
+    #groupName;
+
+    constructor(notes, groupValue, groupName = null) {
+        this.#notes = notes;
+        this.#groupValue = groupValue;
+        this.#groupName = groupName;
+    }
+
+    print() {
+        console.log(`${this.#groupName}: ${this.#groupValue}`);
+        this.#notes.map(note => {
+            if (typeof note === "object") {
+                note.print();
+            } else {
+                console.log(note);
+            };
+        });
+    }
+}
+
+module.exports.subdivideNote = (note, maxValue = "demisemiquaver", grade = 5) => {
+    if (grade < 3 && maxValue === "demisemiquaver") {
+        maxValue = semiquaver;
+    }
+
+    const valueList = Object.keys(this.values);
+    const currentValueIndex = valueList.indexOf(note);
+    const maxValueIndex = valueList.indexOf(maxValue);
+
+    if (note === maxValue) {
+        return new this.RhythmicGroup([note], this.values[note]);
+    }
+
+    let random = Math.floor(Math.random() * 22);
+
+    if (random === 0 && maxValueIndex - currentValueIndex > 1) {
+        return new this.RhythmicGroup([
+            `dotted ${valueList[currentValueIndex + 1]}`,
+            this.subdivideNote(valueList[currentValueIndex + 2], maxValue, grade)
+        ], this.values[note]);
+    } else if (random === 1 && maxValueIndex - currentValueIndex > 1) {
+        return new this.RhythmicGroup([
+            this.subdivideNote(valueList[currentValueIndex + 2], maxValue, grade),
+            `dotted ${valueList[currentValueIndex + 1]}`
+        ], this.values[note]);
+    } else if (random === 2 && maxValueIndex - currentValueIndex > 1) {
+        return new this.RhythmicGroup([
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 1],
+            valueList[currentValueIndex + 2]
+        ], this.values[note]);
+    } else if (random === 3 && grade > 1) {
+        return new this.RhythmicGroup([
+            valueList[currentValueIndex + 1],
+            valueList[currentValueIndex + 1],
+            valueList[currentValueIndex + 1]
+        ], this.values[note], "triplet");
+    } else if (random === 4 && maxValueIndex - currentValueIndex > 1 && grade > 4) {
+        return new this.RhythmicGroup([
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2]
+        ], this.values[note], "quintuplet");
+    } else if (random === 5 && maxValueIndex - currentValueIndex > 1 && grade > 4) {
+        return new this.RhythmicGroup([
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2],
+            valueList[currentValueIndex + 2]
+        ], this.values[note], "sextuplet");
+    } else if (random === 6 && maxValueIndex - currentValueIndex > 2 && grade > 3) {
+        return new this.RhythmicGroup([
+            `double dotted ${valueList[currentValueIndex + 1]}`,
+            this.subdivideNote(valueList[currentValueIndex + 3], maxValue, grade)
+        ], this.values[note]);
+    } else if (random === 7 && maxValueIndex - currentValueIndex > 2 && grade > 3) {
+        return new this.RhythmicGroup([
+            this.subdivideNote(valueList[currentValueIndex + 3], maxValue, grade),
+            `double dotted ${valueList[currentValueIndex + 1]}`
+        ], this.values[note]);
+    } else if (random > 7 && random < 17) {
+        return new this.RhythmicGroup([
+            this.subdivideNote(valueList[currentValueIndex + 1], maxValue, grade),
+            this.subdivideNote(valueList[currentValueIndex + 1], maxValue, grade)
+        ], this.values[note]);
+    } else {
+        return new this.RhythmicGroup([note], this.values[note]);
+    }
+}
