f2c91c1825004637ac06b1fb50a97dc6227a0297
MarcelloDeidda <marcello35@hotmail.it>
2023-07-06 12:36:47 +0100

    New subdivideCompoundNote function

--- music-theory.js
+++ music-theory.js
@@ -1,11 +1,6 @@
+const { createBar } = require("./utils/rhythm/rhythm-functions");
 
-
-const KeyExercises = require("./exercises/scales/key-exercises");
-const Note = require("./utils/notes/note");
-const { calculateIimeSignatureValue, checkBarBeats, getRandomTimeSignature, createBar, subdivideNote } = require("./utils/rhythm/rhythm-utils");
-
-const notes = [new Note("C4", "minim", true), new Note("C4", "crotchet")/*, new Note("C4"), new Note("C4"), new Note("C4")*/];
-
-const group = subdivideNote("semibreve", "semiquaver", 4);
-
-group.print()
+for (let i = 0; i < 1; i++) {
+    const group = createBar([9, 8], "breve", "semiquaver", 5);
+    group.print()
+}
