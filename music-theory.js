67cd9c6b5848430832ed9d212c1ea825c87a5006
MarcelloDeidda <marcello35@hotmail.it>
2023-09-08 23:11:30 +0100

    Added export to main file

--- music-theory.js
+++ music-theory.js
@@ -1,15 +1,5 @@
 const RhythmExercises = require("./exercises/rhythm/RhythmExercises");
 const { createBar, checkBarBeats } = require("./utils/rhythm/rhythm-functions");
 const { subdivideCompoundTernaryNote } = require("./utils/rhythm/rhythm-subdivision");
-/*
-for (let i = 0; i < 1; i++) {
-    try {
-        const group = createBar([7, 8], "minim", "semiquaver");
-        console.log(group)
-    } catch (e) {
-        console.log("ERROR");
-        console.log(e);
-    }
-}*/
 
-console.log(new RhythmExercises(2).addBarLine());
+module.exports = RhythmExercises;
