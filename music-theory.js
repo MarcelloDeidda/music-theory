1bc7bf30c381a25a32d515dc091df93e48c0050d
MarcelloDeidda <marcello35@hotmail.it>
2023-10-26 09:08:09 +0100

    Main file cleanup

--- music-theory.js
+++
@@ -1,5 +0,0 @@
-const RhythmExercises = require("./exercises/rhythm/RhythmExercises");
-const { createBar, checkBarBeats } = require("./utils/rhythm/rhythm-functions");
-const { subdivideCompoundTernaryNote } = require("./utils/rhythm/rhythm-subdivision");
-
-module.exports = RhythmExercises;
