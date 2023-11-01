d83ce7fc13340451f63af25dfb79d9d255d03a67
MarcelloDeidda <marcello35@hotmail.it>
2023-11-01 23:56:58 +0000

    Compiling

---
+++ built/utils/rhythm/rhythm-utils.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.values = exports.timeSignaturesBottomValues = exports.timeSignaturesTopValues = void 0;
+exports.timeSignaturesTopValues = [2, 3, 4, 5, 6, 7, 9, 12];
+exports.timeSignaturesBottomValues = [2, 4, 8, 16];
+exports.values = {
+    breve: 8,
+    semibreve: 4,
+    minim: 2,
+    crotchet: 1,
+    quaver: 0.5,
+    semiquaver: 0.25,
+    demisemiquaver: 0.125
+};
