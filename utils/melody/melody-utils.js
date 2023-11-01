4db25f25d9eec2b70bf9a817aa6ba5468526c73c
MarcelloDeidda <marcello35@hotmail.it>
2023-11-01 22:31:55 +0000

    Removed util folder from root directory

--- utils/melody/melody-utils.js
+++
@@ -1,1 +0,0 @@
-module.exports.startingDegrees = [1, 3, 5];
--- utils/notes/transpose-utils.js
+++
@@ -1,7 +0,0 @@
-module.exports.transposingInstruments = {
-    "trumpet in Bb": "major 2",
-    "clarinet in Bb": "major 2",
-    "clarinet in A": "minor 3",
-    "cor anglais": "perfect 5",
-    "horn in F": "perfect 5"
-}
