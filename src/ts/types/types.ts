740999f6b79056cea4a6da51cf6a1c7f52f615b9
MarcelloDeidda <marcello35@hotmail.it>
2023-11-02 09:39:57 +0000

    New rhythm types

--- src/ts/types/types.ts
+++ src/ts/types/types.ts
@@ -5,3 +5,5 @@ export type directionType = "asc" | "desc";
 export type valueType = "breve" | "semibreve" | "minim" | "crotchet" | "quaver" | "semiquaver" | "demisemiquaver";
 
 export type timeSignatureType = [2 | 3 | 4 | 5 | 6 | 7 | 9 | 12, 2 | 4 | 8 | 16];
+
+export type metreType = "duple" | "triple" | "quadruple" | "irregular";
